c1c8dff6f273d53b779248a8583ab82ab4216204
yat98 <hidayatchandra08@gmail.com>
2019-04-05 22:24:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "azurewebsubapp.database.windows.net";
+    $user = "hidayat";
+    $pass = "Yatadmin_";
+    $db = "[dbo].[Registration]";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
